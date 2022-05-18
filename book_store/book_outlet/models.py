4805f2cc24fea6ab03e3556d2d17a8294cc8c9da
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 15:12:00 -0400

    aggregation methods

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,14 +10,14 @@ class Book (models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author=models.CharField(null=True, max_length=100)
     is_best_selling_book = models.BooleanField(default=False)
-    slug = models.SlugField(default = "", null=False)
+    slug = models.SlugField(default = "", null=False, db_index=True)
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
-        return reverse("book-detail", args = [self.id])
+        return reverse("book-detail", args = [self.slug])
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Book
 from django.http import Http404 
+from django.db.models import Avg
 
 
 # Create your views here.
 def index(request):
     books = Book.objects.all()
+    num_books= books.count()
+    average = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": average
     })
     
-def book_detail(request, id):
+def book_detail(request, slug):
     #try:
        #book=Book.objects.get(pk=id)
     #except:
         #raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     
     return render(request, "book_outlet/book_detail.html", {
         "title":book.title,
