dae64a6656546df5e48db1fc611e7ee93e6e95c5
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 14:18:57 -0400

    slugify

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -11,9 +12,9 @@ class Book (models.Model):
     is_best_selling_book = models.BooleanField(default=False)
     slug = models.SlugField(default = "", null=False)
     
-    def save(self):
-        
-        super().save()
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
     
     def get_absolute_url(self):
         return reverse("book-detail", args = [self.id])
