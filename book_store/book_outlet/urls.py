ecb46b0d68783ca2aecdee7fa357b0bff975e274
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 13:11:30 -0400

    details

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index)
+    path("", views.index),
+    path("<int:id>", views.book_detail, name= "book-detail")
     
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -7,3 +7,13 @@ def index(request):
     return render(request, "book_outlet/index.html", {
         "books": books
     })
+    
+def book_detail(request, id):
+    book=Book.objects.get(pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title":book.title,
+        "author":book.author,
+        "rating":book.rating,
+        "is_bestseller": book.is_best_selling_book
+        
+    })
