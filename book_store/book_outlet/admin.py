99d3b7b10b8d5ccfafc186eb3ea0e11cef3ea10f
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 16:15:18 -0400

    added BookAdmin class

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Book
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+   readonly_fields=("slug",) 
+
+
+admin.site.register(Book, BookAdmin)
