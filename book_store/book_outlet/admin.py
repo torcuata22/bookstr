c4a95a762cc9e00b12bf80258994e33d9d1288a8
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 16:40:00 -0400

    more filters

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -6,6 +6,7 @@ class BookAdmin(admin.ModelAdmin):
    #readonly_fields=("slug",) to avoid error
    prepopulated_fields={"slug": ("title",)} 
    list_filter= ("author","rating")
+   list_display=("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
