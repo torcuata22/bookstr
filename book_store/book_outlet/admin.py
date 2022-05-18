8ddd37108d2eb46904f6535debc7780b05d21aae
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 16:36:28 -0400

    added filters to BookAdmin class

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-
+#create Admin class:
 class BookAdmin(admin.ModelAdmin):
    #readonly_fields=("slug",) to avoid error
    prepopulated_fields={"slug": ("title",)} 
+   list_filter= ("author","rating")
 
 
 admin.site.register(Book, BookAdmin)
