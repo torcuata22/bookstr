99ce135b75de57407715d3248ff89b3bbeded76c
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 15:16:46 -0400

    order_by()

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -6,7 +6,7 @@ from django.db.models import Avg
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("title")
     num_books= books.count()
     average = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
