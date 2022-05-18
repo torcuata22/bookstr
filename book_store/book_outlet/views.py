d230d4a8e174c3bfd2f8cbe269888dcf29866d51
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-18 13:22:43 -0400

    more added views, dynamic tags to new html

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Book
+from django.http import Http404 
+
 
 # Create your views here.
 def index(request):
@@ -9,11 +11,15 @@ def index(request):
     })
     
 def book_detail(request, id):
-    book=Book.objects.get(pk=id)
+    #try:
+       #book=Book.objects.get(pk=id)
+    #except:
+        #raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    
     return render(request, "book_outlet/book_detail.html", {
         "title":book.title,
         "author":book.author,
         "rating":book.rating,
         "is_bestseller": book.is_best_selling_book
-        
     })
