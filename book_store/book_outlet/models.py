e3a40f21113d8b4c0724613f75268e83d712d30d
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-19 12:25:41 -0400

    added new Author model, added FK to Book, deleted old records

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,10 +5,15 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name= models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+        
+
 class Book (models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author=models.CharField(null=True, max_length=100)
+    author=models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_best_selling_book = models.BooleanField(default=False)
     slug = models.SlugField(default = "", null=False, db_index=True)
     
@@ -16,7 +21,7 @@ class Book (models.Model):
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
     #     super().save(*args, **kwargs)
-    
+     
     def get_absolute_url(self):
         return reverse("book-detail", args = [self.slug])
     
